05aa48e76090a3b518d628ab66e80925e7798773
1-E0 <danielalvino1509@gmail.com>
2025-06-12 20:19:18 +0700

    ngilangno comment

--- src/main/java/com/example/bd/HelloApplication.java
+++ src/main/java/com/example/bd/HelloApplication.java
@@ -15,9 +15,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
-        // === PENGATURAN WINDOW UTAMA DI SINI ===
+    
 
-        // 1. Menambahkan Logo/Ikon pada Jendela Aplikasi
         try {
             Image appIcon = new Image(HelloApplication.class.getResourceAsStream("images/chef.png"));
             stage.getIcons().add(appIcon);
@@ -25,16 +24,10 @@ public class HelloApplication extends Application {
             System.err.println("Gagal memuat ikon aplikasi: " + e.getMessage());
         }
 
-        // 2. Mengatur Judul Default Window
+
         stage.setTitle("Aplikasi Catering");
 
-        // 3. Membuat Ukuran Window Tetap (Tidak Bisa Di-resize)
-        //changes\
 
-
-
-
-        
 
 
         stage.setScene(scene);
